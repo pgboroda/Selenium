49ad766d387fc63b722e1fc033a90192d790281d
pgboroda <85794563+pgboroda@users.noreply.github.com>
2021-06-12 17:33:34 +0300

    Доделал методы Delete  и GET.

--- server.go
+++ server.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"encoding/json"
-	"fmt"
 	"io/ioutil"
 	"log"
 	"net/http"
@@ -21,23 +20,20 @@ func main() {
 	//добавить пользователя
 	e.POST("/users", createUser)
 
-	// найти пользователя по Id
+	// найти пользователя по Id, если ID=999,  то вернёт всех пользователей
 	e.GET("/users", findUser)
 
 	// изменить пользователя
 	e.PUT("/users", updateUser)
 
 	// удалить пользователя
-	// e.DELETE("/users", deleteUser)
-
-	//получить всех пользователей
-	//e.GET("/users")
+	e.DELETE("/users", deleteUser)
 
 	e.Logger.Fatal(e.Start(":8080"))
 
 }
 
-// Пуш запрос на добавление юзера
+// POST запрос на добавление юзера
 func createUser(c echo.Context) (err error) {
 	// ловим запрос в Json
 	u := new(User)
@@ -73,7 +69,7 @@ func createUser(c echo.Context) (err error) {
 	return c.JSON(http.StatusOK, u)
 }
 
-// Ищем пользователя по Id
+// GET пользователя по Id
 func findUser(c echo.Context) (err error) {
 
 	// ловим запрос в Json
@@ -89,10 +85,13 @@ func findUser(c echo.Context) (err error) {
 			return c.JSON(http.StatusOK, users[i])
 		}
 	}
+	if u.Id == 999 {
+		return c.JSON(http.StatusOK, users)
+	}
 	return c.JSON(http.StatusBadRequest, "пользователь не существует")
 }
 
-// изменить пользователя
+// PUT изменить пользователя
 func updateUser(c echo.Context) (err error) {
 	// ловим запрос в Json
 	u := new(User)
@@ -131,8 +130,48 @@ func updateUser(c echo.Context) (err error) {
 	return c.JSON(http.StatusBadRequest, "Нет такого пользователя")
 }
 
-// func deleteUser(c echo.Context) error {
-// }
+// DELETE Удалить пользователя
+func deleteUser(c echo.Context) (err error) {
+	// ловим запрос в Json
+	u := new(User)
+	if err = c.Bind(u); err != nil {
+		return echo.NewHTTPError(http.StatusBadRequest, err.Error())
+	}
+	// вызываем файл и преобразуем
+	var users = readJSON()
+	var lessUsers []User
+
+	// ищем пользователя по ID  из полученного запроса
+	for i := 0; i < len(users); i++ {
+		if u.Id == users[i].Id {
+			// если нашли , то запускам цикл снова и перезаписываем массив в новую переменную.
+			// необходимо для того, что бы исключить пустые строки в файле.
+			for i := 0; i < len(users); i++ {
+				if u.Id == users[i].Id {
+					continue
+				} else {
+					lessUsers = append(lessUsers, users[i])
+				}
+
+			}
+			// обновляем файл
+			rawDataOut, err := json.MarshalIndent(&lessUsers, "", "  ")
+			if err != nil {
+				log.Fatal("JSON marshaling failed:", err)
+			}
+
+			err = ioutil.WriteFile(userFilename, rawDataOut, 0)
+			if err != nil {
+				log.Fatal("Cannot write updated settings file:", err)
+			}
+			return c.JSON(http.StatusOK, users[u.Id])
+		} else {
+
+		}
+
+	}
+	return c.JSON(http.StatusBadRequest, "Ошибка в запросе")
+}
 
 // структура файла User
 type (
@@ -142,11 +181,11 @@ type (
 	}
 )
 
-//вытаскиваем и читаем список имён
+//вытаскиваем и читаем список имён из файла
 func readJSON() []User {
 	jsonFile, err := os.Open(userFilename)
 	if err != nil {
-		fmt.Print(err)
+		log.Fatal(err)
 	}
 	defer jsonFile.Close()
 	data, err := ioutil.ReadAll(jsonFile)
